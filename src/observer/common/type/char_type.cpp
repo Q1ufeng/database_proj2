3f6c92aa9cbffc82b72883aa7510db52cfb813ac
Q1ufeng <wangyunzhe2005@163.com>
2024-11-09 11:01:45 +0800

    date attempt 1

--- src/observer/common/type/char_type.cpp
+++ src/observer/common/type/char_type.cpp
@@ -12,6 +12,7 @@ See the Mulan PSL v2 for more details. */
 #include "common/log/log.h"
 #include "common/type/char_type.h"
 #include "common/value.h"
+#include "common/time/datetime.h"
 
 int CharType::compare(const Value &left, const Value &right) const
 {
@@ -29,6 +30,23 @@ RC CharType::set_value_from_str(Value &val, const string &data) const
 RC CharType::cast_to(const Value &val, AttrType type, Value &result) const
 {
   switch (type) {
+    case AttrType::DATES:
+    {
+      result.attr_type_=AttrType::DATES;
+      int y,m,d;
+      if (sscanf(val.value_.pointer_value_,"%d-%d-%d",&y,&m,&d) != 3) {
+        LOG_WARN("invalid date format : %s",val.value_.pointer_value_);
+        return RC::INVALID_ARGUMENT;
+      }
+      int mon[]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+      bool leap=(y%400==0||(y%100&&y%4==0));
+      bool check_ret= y>0&&y<9999 && (m>0)&&(m<=12) && (d>0)&&(d<=((m==2&&leap)?1:0)+mon[m]);
+      if (!check_ret) {
+        LOG_WARN("invalid date format : %s",val.value_.pointer_value_);
+        return RC::INVALID_ARGUMENT;
+      }
+      result.set_date(y,m,d);
+    }break;
     default: return RC::UNIMPLEMENTED;
   }
   return RC::SUCCESS;
@@ -38,6 +56,9 @@ int CharType::cast_cost(AttrType type)
 {
   if (type == AttrType::CHARS) {
     return 0;
+  }
+  if (type==AttrType::DATES){
+    return 1;
   }
   return INT32_MAX;
 }
